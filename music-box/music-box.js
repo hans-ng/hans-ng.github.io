8508301a3ee1af4d26ba7ad6b230b341942e596c
Hans Ng <haunguyen@Haus-MacBook-Pro.local>
2016-11-13 23:02:27 +0700

    Add Music Box project
    Generative Music Box

---
+++ music-box/music-box.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  var canvas = document.getElementById("canvas");
+  var view = new View(canvas);
+  
+  canvas.addEventListener("click", view.handleClick.bind(view), false);
+  
+  setInterval(view.updateDisplay.bind(view), view.frameRate);
+}
